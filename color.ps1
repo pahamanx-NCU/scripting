656cec539f1427c7081495d799c15e12a70e903c
ooioiooGT <97373570+ooioiooGT@users.noreply.github.com>
2022-10-17 01:01:39 -0600

    10/17/2022
    Game rps and guessing color

---
+++ color.ps1
@@ -0,0 +1,67 @@
+﻿<#
+ Program Name : color.ps1
+ Date: 10/11/2022
+ Author: Gilber
+  Corse: CIT361 I, Gilber, affirm that I wrote this script as original work completed by me. 
+ #>
+ 
+ # User needs to guess the computer chosen color.
+ #computer guess the color 
+ $user_count=0
+do{
+    
+    
+     $syscolor = [System.Enum]::getvalues([System.ConsoleColor])
+     $comp_guess = $syscolor|get-random
+
+        #Time truck 
+             $startime = Get-Date
+             #Write-Host $comp_guess
+             $debug = $Fales 
+             if ($debug){Write-Host "Hey cheater, I $comp_guess"}
+          while($user_guess -ne $comp_guess){
+         #User guess 
+             $user_guess = read-Host  "Guess what color did computer chose?"
+
+            if ($user_guess -eq "quit"){
+            break
+            }
+            elseif($user_guess -eq "ask"){
+            Write-Host ($syscolor -join ',')
+            }
+            elseif($user_guess -eq "hint"){
+             $hint = $comp_guess.ToString().substring(0,1)
+            Write-Host " Okay, this is the hint: $hint"
+   
+            if ($hint -eq 'd'){
+            $hint1 = $comp_guess.ToString().Substring(0,5)
+            Write-Host " Okay, this is the hint: $hint1" 
+            }
+
+            }
+
+            elseif($user_guess -notin $syscolor){
+            $user_count += 1
+            "You type in vailed color! Please guess again. Attempts $user_count"
+            }
+            elseif ($comp_guess -ne $user_guess){
+            $user_count = $user_count + 1
+            Write-Host "You guessed " -nonewline
+            Write-Host $user_guess -ForegroundColor $user_guess -NoNewline
+            Write-Host " You guessed wrong! Please guess again. Attempts $user_count"
+            }
+            else{
+            $reslovetime = get-date
+            $time = $reslovetime - $startime
+            $user_count = $user_count - $user_count
+
+            Write-Host "Correct, $comp_guess is my favorite color"
+            "you spent {0:mm}mins  {0:ss}seconds." -f $time 
+            }
+            }
+        $restart = Read-Host "Do you want to play again? type 'y' for yes, or 'n' for no" 
+        $restart -eq 'n' 
+       
+        }while( $restart -eq 'y')
+
+    
---
+++ rps.ps1
@@ -0,0 +1,90 @@
+﻿<#
+ Program Name : rps
+ Date: 10/04/2022
+ Author: Gilber
+  Corse: CIT361 I, Gilber, affirm that I wrote this script as original work completed by me. 
+ #>
+
+
+#pseudocode
+#match: a sequence of game, keep playing until the player wants to quit
+#game: a swquence of rounds, keep playing until either computer or player has won twice
+ $c_win = 0
+ $P_win = 0
+ $round = 0
+
+#rounds: a swquence of rps choices 
+do{
+ Write-Host "Total Score 'User' $total_rounds_usr - 'Computer' $total_rounds_com" -f yellow
+ $comp_score = 0 
+ $user_score = 0
+
+do{
+    Write-Host "User score: $user_score" -f yellow
+    Write-Host "Computer score: $comp_score" -f green
+#   computer chooses
+    $comp_choice = 'r','p','s'|Get-Random
+#   user chooses
+    $per_choice = Read-Host "Choose r for Rock, p for Paper, or s for Scissors?"
+#   determins the winner
+   
+    if ($comp_choice -eq $per_choice){
+    $round += 1
+    Write-Host "Computer $comp_choice The player $per_choice. You are tie"
+    }
+    elseif(($comp_choice -eq 'r') -and ($per_choice -eq 'p')){
+    $user_score += 1
+    $round += 1
+    Write-Host "Computer $comp_choice The player $per_choice. The player win"
+    }
+    elseif(($comp_choice -eq 'r') -and ($per_choice -eq 's')){
+    $comp_score += 1
+    $round += 1
+    Write-Host "Computer $comp_choice The player $per_choice. The computer win"
+    }
+    elseif(($comp_choice -eq 'p') -and ($per_choice -eq 'r')){
+    $comp_score += 1
+    $round += 1
+     Write-Host "Computer $comp_choice The player $per_choice. The computer win"
+    }
+    elseif(($comp_choice -eq 's') -and ($per_choice -eq 'r')){
+    $user_score += 1
+    $round += 1
+    write-host "Computer $comp_choice the player $per_choice. The player win"
+    }
+    elseif(($comp_choice -eq 'p') -and ($per_choice -eq 's')){
+    $user_score += 1
+    $round += 1
+    Write-Host "Computer $comp_choice The player $per_choice. The player win"
+    }
+    elseif(($comp_choice -eq 's') -and ($per_choice -eq 'p')){
+    $comp_score+= 1
+    $round += 1
+    Write-Host "Computer $comp_choice The player $per_choice. The computer win $round $c_win $comp_scor"
+    }
+    
+  }until (($user_score  -eq 2)  -or ($comp_score -eq 2))
+   if ($user_score -eq 2){Write-Host "You won game $user_score - $comp_score" -f green
+        $P_win += 1 
+        Write-Host "Match Score: Computer: $c_win - User: $P_win" -f green
+        } else {
+            $c_win += 1 
+            Write-Host "Match Score: Computer: $c_win - User: $P_win" -f red
+            Write-Host "Computer won game $comp_score - $user_score" -f red
+        } 
+        $restart = Read-Host "Do you want to play again? type 'y' for yes, or 'n' for no" 
+        $restart -eq 'n' 
+}while (
+    $restart -eq 'y'
+)
+if ($c_win -gt $P_win) {
+    Write-Host "Computer Wins" -f red 
+} elseif($P_win -gt $c_win) {
+    Write-Host "User Wins" -f green
+} elseif($P_win -eq $c_win){
+    Write-Host "Tie Game" -f green
+}
+
+Write-Host "FINAL SCORE: COMPUTER: $c_win - USER: $P_win "
+
+#   update cariables, have other round until game is over 
